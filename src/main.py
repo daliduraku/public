d465ba69ba0b0f82b9408e24cc8d7447f15c732d
github_username_here <mesherbetmolla@gmail.com>
2024-08-12 19:13:31 -0400

    Static Site Generator

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from textnode import TextNode
+
+def main():
+    dummy_var = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    
+    print(dummy_var)
+    
+    
+    
+
+if __name__ == "__main__":
+    main()
---
+++ src/test_textnode.py
@@ -0,0 +1,24 @@
+import unittest
+
+from textnode import TextNode
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertEqual(node, node2)
+
+    def test_two(self):
+        node = TextNode("This is a text node", "italic", None)
+        node2 = TextNode("This is a text node", "italic", "https://www.boot.dev")
+        self.assertNotEqual(node, node2)
+    
+    def test_three(self):
+        node = TextNode("This is a text node", "italic", "https://www.boot.dev")
+        node2 = TextNode("This is a text node", "bold", "https://www.boot.dev")    
+        self.assertNotEqual(node, node2)
+
+    
+if __name__ == "__main__":
+    unittest.main()
