e6276cf570837a6145439eda714f2449c43b14d6
github_username_here <mesherbetmolla@gmail.com>
2024-08-13 00:35:51 -0400

    12:36 on a monday

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -24,3 +24,13 @@ class HTMLNode:
     
     def __repr__(self):
         return f"{self.tag}, {self.value}, {self.children}, {self.props}"
+    
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, props)
+    
+    
+    def to_html(self):
+        
+        
