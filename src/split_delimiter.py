e1adcd0212d36dfa9ee1e1a002fc1862a86bdf32
github_username_here <mesherbetmolla@gmail.com>
2024-08-15 17:54:01 -0400

    created the split_nodes_image function

--- src/split_delimiter.py
+++ src/split_delimiter.py
@@ -2,7 +2,7 @@ import re
 from textnode import TextNode
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     
-    text_type_text = "text"; text_type_bold = "bold"; text_type_italic = "italic"; text_type_code = "code"; text_type_link = "link"; text_type_image = "image"
+    text_type_text = "text"; text_type_bold = "bold"; text_type_italic = "italic"; text_type_code = "code"; 
     nodes = ""
     new_text_type = ""
     if delimiter == "*":
@@ -33,3 +33,33 @@ def extract_markdown_links(text):
     pattern = r"(?<!!)\[(.*?)\]\((.*?)\)"
     mathces = re.findall(pattern, text)
     return mathces
+
+def split_nodes_image(old_nodes):
+    text_type_image = "image"
+    text_type_text = "text"
+    nodes_no_img = []
+    new_nodes = []
+
+    for node in old_nodes:
+        images = extract_markdown_images(node.text)
+        
+        if not images:
+            nodes_no_img.append(node)
+        
+        else:
+            for image_alt, image_link in images:
+                sections = node.text.split(f"![{image_alt}]({image_link})", 1)
+
+                if sections[0]:
+                    new_nodes.append(TextNode(sections[0], text_type_text))
+                
+                new_nodes.append(TextNode(f"![{image_alt}]({image_link})", text_type_image))
+                
+                if len(sections) > 1 and sections[1]:
+                    new_nodes.append(TextNode(sections[1], text_type_text))
+    
+    return new_nodes
+        
+         
+    
+    
