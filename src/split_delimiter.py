ddec5b88ca23e4d628a2086166e18028563ac242
github_username_here <mesherbetmolla@gmail.com>
2024-08-16 17:34:44 -0400

    updated the split_delimiter

--- src/split_delimiter.py
+++ src/split_delimiter.py
@@ -13,26 +13,24 @@ from textnode import (
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     
-    nodes = ""
-    new_text_type = ""
-    if delimiter == "*":
-        nodes = old_nodes.split("*")
-        new_text_type = text_type_italic
-    elif delimiter == "**":
-        nodes = old_nodes.split("**")
-        new_text_type = text_type_bold
-    elif delimiter == "`":
-        nodes = old_nodes.split("`")
-        new_text_type = text_type_code
-    
-    text_nodes = []
-    for idx, node in enumerate(nodes):
-        if idx % 2 == 0:
-            text_nodes.append(TextNode(node, text_type_text))
-        else:
-            text_nodes.append(TextNode(node, new_text_type))
-    
-    return text_nodes
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != text_type_text:
+            new_nodes.append(old_node)
+            continue
+        split_nodes = []
+        sections = old_node.text.split(delimiter)
+        if len(sections) % 2 == 0:
+            raise ValueError("Invalid markdown, formatted section not closed")
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i], text_type_text))
+            else:
+                split_nodes.append(TextNode(sections[i], text_type))
+        new_nodes.extend(split_nodes)
+    return new_nodes
 
 def extract_markdown_images(text):
     pattern = r"!\[(.*?)\]\((.*?)\)"
@@ -50,22 +48,17 @@ def split_nodes_image(old_nodes):
         if old_node.text_type != text_type_text:
             new_nodes.append(old_node)
             continue
-        
         original_text = old_node.text
         images = extract_markdown_images(original_text)
-        
         if len(images) == 0:
             new_nodes.append(old_node)
             continue
-        
         for image in images:
             sections = original_text.split(f"![{image[0]}]({image[1]})", 1)
             if len(sections) != 2:
                 raise ValueError("Invalid markdown, image section not closed")
-            
             if sections[0] != "":
                 new_nodes.append(TextNode(sections[0], text_type_text))
-            
             new_nodes.append(
                 TextNode(
                     image[0],
@@ -73,11 +66,9 @@ def split_nodes_image(old_nodes):
                     image[1],
                 )
             )
-
             original_text = sections[1]
         if original_text != "":
             new_nodes.append(TextNode(original_text, text_type_text))
-            
     return new_nodes
 
 
@@ -114,17 +105,3 @@ def split_nodes_link(old_nodes):
     return new_nodes
 
 
-def text_to_textnodes(text):
-    # Handle images and links to avoid splitting 
-    matches_image = extract_markdown_images(text)
-    matches_link = extract_markdown_links(text)
-    
-    list_nodes = split_nodes_image(matches_image)
-    list_nodes += split_nodes_link(matches_link)
-    
-    # Handle formatting delimiters
-    list_nodes += split_nodes_delimiter(text, "**", text_type_bold)
-    list_nodes += split_nodes_delimiter(text, "*", text_type_italic)
-    list_nodes += split_nodes_delimiter(text, "`", text_type_code)
-    
-    return list_nodes
--- src/test_split_delimiter.py
+++ src/test_split_delimiter.py
@@ -6,6 +6,7 @@ from split_delimiter import (
     extract_markdown_links ,
     split_nodes_image,
     split_nodes_link,
+    text_to_textnodes,
 )
 
 from textnode import ( TextNode,
@@ -99,6 +100,29 @@ class TestSplitDelimiter(unittest.TestCase):
             ],
             new_nodes,
         )
+        
+    def test_texttotextnode(self):
+       text = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+       node = text_to_textnodes(text)
+       self.assertListEqual(
+           [
+                TextNode("This is ", text_type_text),
+                TextNode("text", text_type_bold),
+                TextNode(" with an ", text_type_text),
+                TextNode("italic", text_type_italic),
+                TextNode(" word and a ", text_type_text),
+                TextNode("code block", text_type_code),
+                TextNode(" and an ", text_type_text),
+                TextNode("obi wan image", text_type_image, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", text_type_text),
+                TextNode("link", text_type_link, "https://boot.dev"),
+           ],
+           node,
+       )
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
