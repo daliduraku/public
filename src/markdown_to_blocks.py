8613bcf6ad7c3a4cb1abb0a4139f4a3eda3274ed
github_username_here <mesherbetmolla@gmail.com>
2024-08-16 22:22:01 -0400

    Fixed the bugs in my markdown_to_blocks function

--- src/markdown_to_blocks.py
+++ src/markdown_to_blocks.py
@@ -3,11 +3,20 @@ import re
 def markdown_to_blocks(markdown):
     blank_line_regex = r"(?:\r?\n){2,}"
     
-    markdown_strings = re.split(blank_line_regex, markdown.strip())
+    markdown_strings = re.split(r"(?:\r?\n){2,}", markdown.strip())
     final_markdownstrings = []
+
     for string in markdown_strings:
-        if string != "":
-            final_markdownstrings.append(string)
+        cleaned_string = string.split('\n')
+        
+        cleaned_lines = []
+        for string in cleaned_string:
+            cleaned_strings = string.strip()
+            cleaned_lines.append(cleaned_strings)
+        final_string =  "\n".join(cleaned_lines)
+        
+        if final_string != "":
+            final_markdownstrings.append(final_string)   
         
     
     return final_markdownstrings
