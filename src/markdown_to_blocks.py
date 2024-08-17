7cd97f1502ab522aa9cc6b1620ed476eee7ffd55
github_username_here <mesherbetmolla@gmail.com>
2024-08-17 16:52:41 -0400

    Created the block_to_block_type function

--- src/markdown_to_blocks.py
+++ src/markdown_to_blocks.py
@@ -1,5 +1,12 @@
 import re
 
+# creating a tuple with the startwith
+start_of_headings = ("# ", "## ", "### ", "#### ", "##### ", "###### ",)
+start_of_code ="```"
+start_of_line = ">"
+start_of_unordered = ("* ", "- ")
+start_of_paragraph = "paragraph"
+
 def markdown_to_blocks(markdown):
     blank_line_regex = r"(?:\r?\n){2,}"
     
@@ -20,6 +27,43 @@ def markdown_to_blocks(markdown):
         
     
     return final_markdownstrings
+
+# function takes a string in as the form single block of markdown text
+def block_to_block_type(markdown):
+
+    # checking 
+    if markdown.startswith(start_of_headings):
+        return "heading"
     
+    # spliting the string into lines
+    lines = markdown.splitlines()
     
+    if lines[0].startswith(start_of_code) and lines[-1].endswith("```"):
+            return "code"
+    
+    # checking uniformity 
+    all_quote = all(line.startswith(start_of_line) for line in lines)
+
+    if all_quote:
+        return "quote"
+
+     # checking uniformity 
+    all_unordered = all(line.startswith(start_of_unordered) for line in lines)
+
+    if all_unordered:
+        return "unordered_list"
+    
+    current_number = 1
+    is_ordered = True
+    for line in lines:
+        if line.startswith(f"{current_number}. "):
+            current_number += 1
+        else:
+            is_ordered = False
+            break
+    
+    if is_ordered:
+        return "ordered_list"
+    else:
+        return "paragraph"
     
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -2,6 +2,14 @@ import unittest
 import re
 from markdown_to_blocks import(
     markdown_to_blocks,
+    block_to_block_type,
+    start_of_code,
+    start_of_headings,
+    start_of_line,
+    start_of_unordered,
+    start_of_paragraph,
+    
+    
     
 )
 
@@ -52,7 +60,19 @@ class TestMarkdown(unittest.TestCase):
                 "* This is a list\n* with items",
             ],
         )   
-        
+    def test_block_to_block_types(self):
+        #block = "# heading"
+        #self.assertEqual(block_to_block_type(block), start_of_headings)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), start_of_code)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), start_of_line)
+        block = "* list\n* items"
+        self.assertEqual(block_to_block_type(block), start_of_unordered)
+        #block = "1. list\n2. items"
+        #self.assertEqual(block_to_block_type(block), block_type_olist)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), start_of_paragraph)    
 if __name__ == "__main__":
     unittest.main()      
         
